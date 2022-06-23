cd7e4de9fb97e062c06521f8ae932b4dd2b88cea
carmen0810 <83844544+carmen0810@users.noreply.github.com>
2022-06-23 10:53:21 -0500

    cambios

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,21 @@
 // este sera un login
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <div className="login">
+        <input placeholder='correo electronico' />
+      </div>
+      <div>
+        <input placeholder='contraseña' />
+      </div>
+      <div>
+        <button>iniciar sesión</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default Login;
